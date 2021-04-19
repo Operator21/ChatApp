46fb4aab5fe8ec07b936b67f97556ace01e2ea86
Stanislav Zdych <stanik.zdych@gmail.com>
2021-04-19 21:15:49 +0200

    welcome ui

---
+++ welcomepage.php
@@ -0,0 +1,20 @@
+<main class="welcome">
+    <div class="splitter">
+        <div>
+            <h1 class="width-250">Buďte spolu, ale dostatečně daleko</h1>
+            <h4 class="width-250">Díky ChatApp nemusíme přemýšlet nad tím jestli vám někdo krade osobní údaje, u nás máte jistotu, že ano.</h4>
+            <div style="margin-top: 40px; width: 250px;">
+                <?php 
+                    include_once("form_login.php");
+                    include_once("form_register.php");
+                ?>
+            </div>
+        </div>
+        <div>
+            <img src="img/devices.png">
+        </div>
+    </div>
+    <footer>
+        <h4>Stanislav Zdych &copy; 2021</h4>
+    </footer>
+</main>

819039bd369ce1c03faa9f5f83e3b46e4f1bc9ad
Stanislav Zdych <stanik.zdych@gmail.com>
2024-03-22 17:16:33 +0100

    licence updated

--- form_login.php
+++ form_login.php
@@ -1,3 +1,23 @@
+<?php
+/**
+* Copyright (C) 2021 Stanislav Zdych
+* 
+* This file is part of the ChatApp.
+* 
+* ChatApp is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+?>
 <a href="#loginModal" class="button" rel="modal:open">Přihlásit se</a>
 <form method="post" id="loginModal" class="modal" action="javascript:Login()">
     <h4>Přihlášení</h4>
--- template_data.php
+++ template_data.php
@@ -1,4 +1,24 @@
 <?php
+/**
+* Copyright (C) 2021 Stanislav Zdych
+* 
+* This file is part of the ChatApp.
+* 
+* ChatApp is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+* 
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+?>
+<?php
 $photos = [
     "https://spic.one/wp-content/uploads/2018/07/7ae96e9407a398d51f4618a244e41329.jpg",
     "https://i.pinimg.com/originals/7e/55/fb/7e55fb591f6851c8d204a253f77bdcfb.jpg",
